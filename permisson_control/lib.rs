d1f0be8f276c1d7d246dfb7ff4b24f0eafb49c26
NguyenVietThanh <nguyenvietthanh18112002@gmail.com>
2024-12-03 14:07:31 +0700

    add multisig access control
    Access control, separate admins and speculator
    Admins can perform  system based transaction.
    Speculator are the user, only limited to a number of transactions

--- permisson_control/lib.rs
+++ permisson_control/lib.rs
@@ -1,9 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
-mod permisson_control {
+pub mod permisson_control {
     use ink::prelude::vec;
-    use ink::storage::Mapping;
     use ink::{
         primitives::Key,
         storage::{traits::ManualKey, Lazy, Mapping, StorageVec},
@@ -14,14 +13,14 @@ mod permisson_control {
     type TransactionId = u32;
     type MAX_ADMINS = u32;
 
+    const MAX_ADMINS: u32 = 50;
+
     #[ink(storage)]
     pub struct PermissonControl {
         confirm_transactions: Mapping<(TransactionId, AccountId), ()>,
-        confirm_transactions_count: Mapping<TransactionId, u32>,
-        transactions: Mapping<TransactionId, Transaction>,
-        transaction_list: TransactionList,
-        admins_list: StorageVec<AccountId>,
-        is_admins: Mapping<AccountId, ()>,
+        // redudant information to speed up check if the transaction is confirmed
+        confirm_transactions_index: Mapping<TransactionId, u32>,
+        admins: Mapping<AccountId, ()>,
         min_admins_requirement: u32,
     }
     #[derive(Debug, Clone, Decode, Encode, PartialEq, Eq, TypeInfo)]
@@ -41,15 +40,24 @@ mod permisson_control {
         next_id: TransactionId,
     }
 
+    #[derive(Debug, Clone, Decode, Encode, TypeInfo)]
+    #[cfg_attr(feature = "std", derive(ink::storage::traits::StorageLayout))]
+    pub enum PermissionError {
+        NotAdmin,
+        InvalidOperation,
+        NoDataFound,
+    }
+
+    type PermissonResult<T> = Result<T, PermissionError>;
+
+    #[ink(event)]
+    pub struct Event {}
     impl Default for PermissonControl {
         fn default() -> Self {
             Self {
                 confirm_transactions: Mapping::default(),
-                confirm_transactions_count: Mapping::default(),
-                transactions: Mapping::default(),
-                transaction_list: Default::default(),
-                admins_list: StorageVec::default(),
-                is_admins: Mapping::default(),
+                confirm_transactions_index: Mapping::default(),
+                admins: Mapping::default(),
                 min_admins_requirement: Default::default(),
             }
         }
@@ -65,108 +73,56 @@ mod permisson_control {
                 u32::try_from(admins.len()).expect("the admins input should not be empty"),
             );
             for admin in admins.iter() {
-                _init_contract.is_admins.insert(admin, &());
+                let _ = _init_contract.admins.try_insert(admin, &());
             }
             _init_contract
         }
 
         #[ink(message)]
-        pub fn flip(&mut self) {}
+        pub fn add_admins(&mut self, new_admins: AccountId) -> PermissonResult<()> {
+            self.ensure_from_wallet();
+            self.ensure_no_admin(&new_admins);
+            let _ = self.admins.try_insert(new_admins, &());
+            self.env().emit_event(Event {});
+            Ok(())
+        }
 
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            true
+        pub fn remove_admins(&mut self, removed_admins: AccountId) -> PermissonResult<()> {
+            self.ensure_from_wallet();
+            self.ensure_is_admin(&removed_admins);
+            match self.admins.get(removed_admins) {
+                Some(val) => self.admins.remove(removed_admins),
+                None => return Err(PermissionError::NoDataFound),
+            }
+            Ok(())
         }
-
+        #[ink(message)]
+        pub fn replace_admins(&mut self, from: AccountId, to: AccountId) -> PermissonResult<()> {
+            self.ensure_from_wallet();
+            self.ensure_is_admin(&from);
+            self.ensure_no_admin(&to);
+            match self.admins.get(from) {
+                Some(val) => {
+                    self.admins.remove(from);
+                    self.admins.insert(to, &());
+                }
+                None => return Err(PermissionError::InvalidOperation),
+            }
+            Ok(())
+        }
         fn ensure_min_of_require_admins(&mut self, admins: u32) {
             self.min_admins_requirement = admins * 2 / 3;
-            assert!(0 < self.min_admins_requirement && self.min_admins_requirement < admins)
+            assert!(2 < self.min_admins_requirement && self.min_admins_requirement < admins)
+        }
+        fn ensure_from_wallet(&self) {
+            assert_eq!(self.env().caller(), self.env().account_id())
+        }
+        fn ensure_no_admin(&self, account_id: &AccountId) {
+            assert!(!self.admins.contains(account_id))
+        }
+        fn ensure_is_admin(&self, account_id: &AccountId) {
+            assert!(self.admins.contains(account_id))
         }
     }
-
-    // #[cfg(test)]
-    //  mod tests {
-    //      use super::*;
-    //
-    //      #[ink::test]
-    //      fn default_works() {
-    //          let permisson_control = PermissonControl::default();
-    //          assert_eq!(permisson_control.get(), false);
-    //      }
-    //
-    //      #[ink::test]
-    //      fn it_works() {
-    //          let mut permisson_control = PermissonControl::new(false);
-    //          assert_eq!(permisson_control.get(), false);
-    //          permisson_control.flip();
-    //          assert_eq!(permisson_control.get(), true);
-    //      }
-    //  }
-    //
-    //
-    // #[cfg(all(test, feature = "e2e-tests"))]
-    //  mod e2e_tests {
-    //      /// Imports all the definitions from the outer scope so we can use them here.
-    //      use super::*;
-    //
-    //      /// A helper function used for calling contract messages.
-    //      use ink_e2e::ContractsBackend;
-    //
-    //      /// The End-to-End test `Result` type.
-    //      type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-    //
-    //      /// We test that we can upload and instantiate the contract using its default constructor.
-    //      #[ink_e2e::test]
-    //      async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-    //          // Given
-    //          let mut constructor = PermissonControlRef::default();
-    //
-    //          // When
-    //          let contract = client
-    //              .instantiate("permisson_control", &ink_e2e::alice(), &mut constructor)
-    //              .submit()
-    //              .await
-    //              .expect("instantiate failed");
-    //          let call_builder = contract.call_builder::<PermissonControl>();
-    //
-    //          // Then
-    //          let get = call_builder.get();
-    //          let get_result = client.call(&ink_e2e::alice(), &get).dry_run().await?;
-    //          assert!(matches!(get_result.return_value(), false));
-    //
-    //          Ok(())
-    //      }
-    //
-    //      /// We test that we can read and write a value from the on-chain contract.
-    //      #[ink_e2e::test]
-    //      async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-    //          // Given
-    //          let mut constructor = PermissonControlRef::new(false);
-    //          let contract = client
-    //              .instantiate("permisson_control", &ink_e2e::bob(), &mut constructor)
-    //              .submit()
-    //              .await
-    //              .expect("instantiate failed");
-    //          let mut call_builder = contract.call_builder::<PermissonControl>();
-    //
-    //          let get = call_builder.get();
-    //          let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-    //          assert!(matches!(get_result.return_value(), false));
-    //
-    //          // When
-    //          let flip = call_builder.flip();
-    //          let _flip_result = client
-    //              .call(&ink_e2e::bob(), &flip)
-    //              .submit()
-    //              .await
-    //              .expect("flip failed");
-    //
-    //          // Then
-    //          let get = call_builder.get();
-    //          let get_result = client.call(&ink_e2e::bob(), &get).dry_run().await?;
-    //          assert!(matches!(get_result.return_value(), true));
-    //
-    //          Ok(())
-    //      }
-    //  }
 }
